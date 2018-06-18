7517ec5793d81a9db85e967ea39552a8400ce527
Abdullah-A-Elsayed <abdullah.ali26@hotmail.com>
2018-06-18 05:05:24 +0200

    asu theme initiated

---
+++ Themes/ASU/views/layouts/master.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html class="wide wow-animation scrollTo desktop landscape rd-navbar-static-linked">
+<head lang="{{ LaravelLocalization::setLocale() }}">
+    <meta charset="UTF-8">
+    @section('meta')
+        <meta name="description" content="@setting('core::site-description')" />
+        <meta name="format-detection" content="telephone=no">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta name="keywords" content="intense web design multipurpose template">
+    @show
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <title>
+        @section('title')@setting('core::site-name')@show
+    </title>
+    <link rel="shortcut icon" href="{{ Theme::url('favicon.ico') }}">
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300italic,300,400italic,600,700%7CMerriweather:400,300,300italic,400italic,700,700italic">
+    <!-- {!! Theme::style('css/main.css') !!} -->
+    {!! Theme::style('css/style.css') !!}
+    @stack('css-stack')
+        <!--[if lt IE 10]>
+    <div style="background: #212121; padding: 10px 0; box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3); clear: both; text-align:center; position: relative; z-index:1;"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <script src="js/html5shiv.min.js"></script>
+        <![endif]-->
+  </head>
+  <body>
+    <!-- Page-->
+
+@auth
+    @include('partials.admin-bar')
+@endauth
+    @include('partials.navigation')
+<div class="page text-center">
+@include('partials.header')
+<main class="page-content">
+    @yield('content')
+</main>
+@include('partials.footer')
+</div>
+
+@yield('scripts')
+
+<?php if (Setting::has('core::analytics-script')): ?>
+    {!! Setting::get('core::analytics-script') !!}
+<?php endif; ?>
+@stack('js-stack')
+</body>
+</html>
---
+++ Themes/ASU/views/partials/footer.blade.php
@@ -0,0 +1,121 @@
+<!-- Page Footer-->
+      <!-- Corporate footer-->
+      <footer class="page-footer">
+        <div class="shell-wide">
+          <div class="hr bg-gray-light"></div>
+        </div>
+        <div class="shell section-60">
+          <div class="range range-lg-justify range-xs-center">
+            <div class="cell-md-3 cell-lg-2">
+              <!--Footer brand--><a class="reveal-inline-block" href="index.html"><img width='170' height='172' src="{{ Theme::url('images/logo-170x1722.png') }}" alt='uni logo'/>
+                <div>
+                  <h6 class="barnd-name text-bold offset-top-25">Ain Shams</h6>
+                </div>
+                <div>
+                  <p class="brand-slogan text-gray text-italic font-accent">University</p>
+                </div></a>
+            </div>
+            <div class="cell-xs-10 cell-md-5 cell-lg-4 text-lg-left offset-top-50 offset-md-top-0">
+              <h6 class="text-bold">Contact us</h6>
+              <div class="text-subline"></div>
+              <div class="offset-top-30">
+                <ul class="list-unstyled contact-info list">
+                  <li>
+                    <div class="unit unit-horizontal unit-middle unit-spacing-xs">
+                      <div class="unit-left"><span class="icon mdi mdi-phone text-middle icon-xs text-madison"></span></div>
+                      <div class="unit-body"><a class="text-dark" href="tel:#">26831231,</a> <a class="reveal-block reveal-md-inline-block text-dark" href="tel:#">26831417,</a><a class="reveal-block reveal-md-inline-block text-dark" href="tel:#">26831474</a>
+                      </div>
+                    </div>
+                  </li>
+                  <li class="offset-top-15">
+                    <div class="unit unit-horizontal unit-middle unit-spacing-xs">
+                      <div class="unit-left"><span class="icon mdi mdi-map-marker text-middle icon-xs text-madison"></span></div>
+                      <div class="unit-body text-left"><a class="text-dark" href="#">El-Khalyfa El-Ma'moun Street Abbasya, Cairo , Egypt</a></div>
+                    </div>
+                  </li>
+                  <li class="offset-top-15">
+                    <div class="unit unit-horizontal unit-middle unit-spacing-xs">
+                      <div class="unit-left"><span class="icon mdi mdi-email-open text-middle icon-xs text-madison"></span></div>
+                      <div class="unit-body"><a href="mailto:#">Info@asu.edu.eg</a></div>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              <div class="offset-top-15 text-left">
+                <ul class="list-inline list-inline-xs list-inline-madison">
+                    <li><a class="icon icon-xxs fa-facebook icon-circle icon-gray-light-filled" href="https://www.facebook.com/AinShams.University.EG/"></a></li>
+                    <li><a class="icon icon-xxs fa-twitter icon-circle icon-gray-light-filled" href="https://twitter.com/ainshamsegypt"></a></li>
+                    <li><a class="icon icon-xxs fa fa-google-plus icon-circle icon-gray-light-filled google-plusH" href="https://plus.google.com/u/0/110327766728601587217 "></a></li>
+                    <li><a class="icon icon-xxs fa fa-wordpress icon-circle icon-gray-light-filled wordPressH " href="https://ainshamsuniversity14.wordpress.com/"></a></li>
+                    <li><a class="icon icon-xxs fa fa-file-code-o icon-circle icon-gray-light-filled wixH" href="http://ainshamsuni.wixsite.com/asu-ainshams"></a></li>
+                    <li><a class="icon icon-xxs fa fa-file-code-o icon-circle icon-gray-light-filled blogH" href="https://asuniversity.blogspot.com.eg/"></a></li>
+                 </ul>
+              </div>
+            </div>
+            <div class="cell-xs-10 cell-md-8 cell-lg-4 text-lg-left offset-top-50 offset-lg-top-0">
+              <h6 class="text-bold">Newsletter</h6>
+              <div class="text-subline"></div>
+              <div class="offset-top-30 text-left">
+                <p>Enter your email address to get the latest University news, special events and student activities delivered right to your inbox.</p>
+              </div>
+              <div class="offset-top-10">
+                      <form class="rd-mailform form-subscribe" data-form-output="form-subscribe-footer" data-form-type="subscribe" method="post" action="bat/rd-mailform.php">
+                        <div class="form-group">
+                          <div class="input-group input-group-sm">
+                            <input class="form-control" placeholder="Your e-mail" type="email" name="email" data-constraints="@Required @Email"><span class="input-group-btn">
+                              <button class="btn btn-sm btn-primary" type="submit">Subscribe</button></span>
+                          </div>
+                        </div>
+                        <div class="form-output" id="form-subscribe-footer"></div>
+                      </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bg-madison context-dark">
+          <div class="shell text-md-left section-5">
+            <p>&copy; <span id="copyright-year"></span> All Rights Reserved Terms of Ain Shams University and <a href="privacy.html">Privacy Policy</a></p>
+          </div>
+        </div>
+      </footer>
+    </div>
+    <!-- Global Mailform Output-->
+    <div class="snackbars" id="form-output-global"></div>
+    <!-- PhotoSwipe Gallery-->
+    <div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="pswp__bg"></div>
+      <div class="pswp__scroll-wrap">
+        <div class="pswp__container">
+          <div class="pswp__item"></div>
+          <div class="pswp__item"></div>
+          <div class="pswp__item"></div>
+        </div>
+        <div class="pswp__ui pswp__ui--hidden">
+          <div class="pswp__top-bar">
+            <div class="pswp__counter"></div>
+            <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
+            <button class="pswp__button pswp__button--share" title="Share"></button>
+            <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
+            <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
+            <div class="pswp__preloader">
+              <div class="pswp__preloader__icn">
+                <div class="pswp__preloader__cut">
+                  <div class="pswp__preloader__donut"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+            <div class="pswp__share-tooltip"></div>
+          </div>
+          <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button>
+          <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)"></button>
+          <div class="pswp__caption">
+            <div class="pswp__caption__center"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Java script-->
+    <script src="{{Theme::url('js/core.min.js')}}"></script>
+    <script src="{{Theme::url('js/script.js')}}"></script>
