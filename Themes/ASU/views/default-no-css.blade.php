e9dd29cfb31da805bf8195a8ea538a0386101afe
Bola Erian <Bola Erian>
2018-08-08 14:55:44 -0700

    add no-style-template

---
+++ Themes/ASU/views/default-no-css.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.master')
+
+@section('title')
+    {{ $page->title }} | @parent
+@stop
+@section('meta')
+    <meta name="title" content="{{ $page->meta_title}}" />
+    <meta name="description" content="{{ $page->meta_description }}" />
+@stop
+
+@section('css')
+    {!! Theme::style('css/no-style.css') !!}
+@stop
+@section('header')
+      @include('partials.default-header')
+@stop
+
+@section('content')
+    <!-- <div class="row"> -->
+        {!! $page->body !!}
+    <!-- </div> -->
+@stop
+
+@section('js')
+     <script src="{{Theme::url('js/core.min.js')}}"></script>
+    <script src="{{Theme::url('js/script.js')}}"></script>
+@stop
